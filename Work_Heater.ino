858be03475f670fc5bd0f9ba94994cffac51109c
Brian Falor <brian.falor@gmail.com>
2017-12-07 07:41:53 -0700

    First commit

---
+++ Work_Heater.ino
@@ -0,0 +1,133 @@
+#include <IRremote.h>
+#include <Adafruit_SleepyDog.h> 
+#include <Adafruit_SSD1306.h>
+#include "Wire.h"
+#include "SPI.h"
+
+#define OLED 0x3C
+//#define OLED 0x57
+#define DS3231_I2C_ADDRESS 0x68
+#define LED   5
+#define OLED_RESET 10
+
+#define HEAT  0xFF23DC
+#define HEAT2 0x690CEFE0
+#define TV    0x20DF10EF
+
+IRsend irsend;
+Adafruit_SSD1306 display(OLED_RESET);
+int BUTTON = 9;
+int SWITCH = 12;
+
+void printTime();
+void getTime();
+String B_minute;
+String B_second;
+String B_hour; 
+int B_dayOfMonth;
+int B_month;
+int B_year;
+int buttonState;
+int switchState;
+
+int flag = 0;
+String check  = ""; 
+String target = "0650";
+
+void setup(){
+
+  Wire.begin();
+  Serial.begin(9600);
+  pinMode(BUTTON, INPUT);
+  pinMode(SWITCH, INPUT);
+  pinMode(LED, OUTPUT);
+
+  display.begin(SSD1306_SWITCHCAPVCC, OLED);  // initialize with the I2C addr 0x3C (for the 128x32)
+  display.clearDisplay();
+  display.setTextSize(.4);
+  display.setTextColor(WHITE);
+}
+
+
+void loop() {
+  switchState = digitalRead(SWITCH); 
+   
+  if (switchState == HIGH){
+    getTime(); 
+    Serial.println("HIGH");
+    digitalWrite(LED, HIGH);
+    
+    display.clearDisplay(); 
+
+    display.setCursor(0, 5);
+    display.print("Time: ");
+    display.print(B_hour);
+    display.print(":");
+    display.print(B_minute); 
+
+    display.setCursor(0, 20);
+    display.print("Set:  ");
+    display.print(target);
+    
+    display.display();
+
+    buttonState = digitalRead(BUTTON);
+
+    if (buttonState == HIGH){
+         Serial.println("Sending IR Code"); 
+         irsend.sendNEC(HEAT, 32); //send IR code
+         delay(300);
+    }
+    
+  }
+  else{
+    Serial.println("LOW");
+    digitalWrite(LED, LOW);
+    display.clearDisplay(); 
+    display.display();
+    
+    Watchdog.sleep();
+    delay(200); 
+   
+    getTime(); 
+   //printTime();
+    
+    check.concat(B_hour);
+    check.concat(B_minute);
+  
+  
+    Serial.print("Target = ");
+    Serial.println(target); 
+    Serial.print("Check = ");
+    Serial.println(check); 
+  
+    if(check == target){
+      Serial.println("Target Met");
+      flag = 1;
+      digitalWrite(LED, HIGH);
+    }
+  
+  
+    if(flag == 1){
+      delay(60010); //wait for just over a minuet to ensure the flag can't be set again.
+      irsend.sendNEC(HEAT, 32); //send IR code
+      delay(300);
+      //digitalWrite(LED, LOW); 
+      flag = 0;
+    }
+  
+     check = ""; //reset the check variable.
+  
+     Serial.print("flag = ");
+     Serial.println(flag); 
+     delay(200);
+  }
+}
+
+
+
+
+
+
+
+
---
+++ clock.ino
@@ -0,0 +1,139 @@
+// Convert normal decimal numbers to binary coded decimal
+byte decToBcd(byte val)
+{
+  return( (val/10*16) + (val%10) );
+}
+
+
+// Convert binary coded decimal to normal decimal numbers
+byte bcdToDec(byte val)
+{
+  return( (val/16*10) + (val%16) );
+}
+
+
+
+void readDS3231time(byte *second,
+byte *minute,
+byte *hour,
+byte *dayOfWeek,
+byte *dayOfMonth,
+byte *month,
+byte *year)
+{
+  Wire.beginTransmission(DS3231_I2C_ADDRESS);
+  Wire.write(0); // set DS3231 register pointer to 00h
+  Wire.endTransmission();
+  Wire.requestFrom(DS3231_I2C_ADDRESS, 7);
+  // request seven bytes of data from DS3231 starting from register 00h
+  *second = bcdToDec(Wire.read() & 0x7f);
+  *minute = bcdToDec(Wire.read());
+  *hour = bcdToDec(Wire.read() & 0x3f);
+  *dayOfWeek = bcdToDec(Wire.read());
+  *dayOfMonth = bcdToDec(Wire.read());
+  *month = bcdToDec(Wire.read());
+  *year = bcdToDec(Wire.read());
+}
+
+
+
+void displayTime()
+{
+  Serial.println("\n");
+  
+  byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
+  // retrieve data from DS3231
+  readDS3231time(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
+  // send it to the serial monitor
+  Serial.print(hour, DEC);
+  // convert the byte variable to a decimal number when displayed
+  Serial.print(":");
+  if (minute<10)
+  {
+    Serial.print("0");
+  }
+  Serial.print(minute, DEC);
+  Serial.print(":");
+  if (second<10)
+  {
+    Serial.print("0");
+  }
+  Serial.print(second, DEC);
+  Serial.print(" ");
+  Serial.print(dayOfMonth, DEC);
+  Serial.print("/");
+  Serial.print(month, DEC);
+  Serial.print("/");
+  Serial.print(year, DEC);
+  Serial.print(" Day of week: ");
+  
+  switch(dayOfWeek){
+  case 1:
+    Serial.println("Sunday");
+    break;
+  case 2:
+    Serial.println("Monday");
+    break;
+  case 3:
+    Serial.println("Tuesday");
+    break;
+  case 4:
+    Serial.println("Wednesday");
+    break;
+  case 5:
+    Serial.println("Thursday");
+    break;
+  case 6:
+    Serial.println("Friday");
+    break;
+  case 7:
+    Serial.println("Saturday");
+    break;
+  }
+}
+
+void getTime(){
+ byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
+  // retrieve data from DS3231
+  readDS3231time(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
+  
+   B_second = (second);
+   if(second < 10){ 
+      B_second = ""; 
+      B_second.concat("0");
+      B_second.concat(second);
+   }
+   
+   B_minute = (minute);
+   if(minute < 10){ 
+      B_minute = ""; 
+      B_minute.concat("0");
+      B_minute.concat(minute);
+   }
+
+   B_hour = (hour); 
+   if(hour < 10){ 
+      B_hour = ""; 
+      B_hour.concat("0");
+      B_hour.concat(hour);
+   }
+
+   B_dayOfMonth = dayOfMonth;
+   B_month = month;
+   B_year = year;   
+}
+
+void printTime(){
+  getTime();
+  Serial.print(B_hour);
+  Serial.print(":");
+  
+  Serial.print(B_minute);
+  Serial.print(":");
+  
+  Serial.print(B_second);
+  Serial.println();
+
+  delay(1000);
+}
+
